1d00ee7afdf39b5684f3e0f0691afc7a709b5fab
AlgoWebBot <shimulzahan636@gmail.com>
2023-11-05 05:32:05 +0600

    initial setup

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+
+console.log(process.env.DB_NAME)
+console.log(process.env.DB_PASS)
+
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@shimulclaster1.85diumq.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+        // .limit(6)
+
+        const allFoods = client.db("communityFoodSharingDB").collection('allFoods');
+
+        app.get('/all-foods', async (req, res) => {
+            const count = await allFoods.estimatedDocumentCount();
+            const result = await allFoods.find().sort({ quantity: -1 }).toArray();
+            res.send(result);
+        })
+
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.listen(port, () => {
+    console.log(`Server running at ${port}`)
+})
