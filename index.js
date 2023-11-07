06ffb2cc83117cbff2e8aef2fa1546ba5be5f75a
AlgoWebBot <shimulzahan636@gmail.com>
2023-11-08 00:19:55 +0600

    canel request api update

--- index.js
+++ index.js
@@ -135,6 +135,12 @@ async function run() {
             res.send(result);
         })
 
+        app.delete('/delete-requested-food/:id', async (req, res) => {
+            const id = req.params.id;
+            const result = await requestFoods.deleteOne({ _id: new ObjectId(id) });
+            res.send(result);
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
