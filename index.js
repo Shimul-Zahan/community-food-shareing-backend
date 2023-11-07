9970cc89e513f6c9acde2d714b811f3e1321b2a8
AlgoWebBot <shimulzahan636@gmail.com>
2023-11-07 23:04:08 +0600

    delete api update

--- index.js
+++ index.js
@@ -106,6 +106,12 @@ async function run() {
             res.send(result);
         })
 
+        app.delete('/delete-food/:id', async (req, res) => {
+            const id = req.params.id;
+            const result = await allFoods.deleteOne({ _id: new ObjectId(id) });
+            res.send(result);
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
