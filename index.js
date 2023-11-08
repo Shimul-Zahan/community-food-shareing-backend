674b3414e17ade2ad4776db98a56df61cd88a983
AlgoWebBot <shimulzahan636@gmail.com>
2023-11-08 06:19:01 +0600

    jwt verify update

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const jwt = require('jsonwebtoken');
+const cookieparse = require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
@@ -8,8 +10,14 @@ require('dotenv').config();
 // console.log(process.env.DB_PASS)
 
 const cors = require('cors');
-app.use(cors());
+app.use(cors({
+    origin: [
+        'http://localhost:5173'
+    ],
+    credentials: true,
+}));
 app.use(express.json());
+app.use(cookieparse())
 
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@shimulclaster1.85diumq.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -21,6 +29,21 @@ const client = new MongoClient(uri, {
     }
 });
 
+const security = (req, res, next) => {
+    const token = req.cookies?.token
+    // console.log("cookies token",token)
+    if (!token) {
+        return res.status(401).send({ message: 'Unauthorized access' })
+    }
+    jwt.verify(token, process.env.SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({message: 'Access forbiden'})
+        }
+        req.decode = decoded
+        next();
+    })
+}
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -30,12 +53,12 @@ async function run() {
         const allFoods = client.db("communityFoodSharingDB").collection('allFoods');
         const requestFoods = client.db("communityFoodSharingDB").collection('requestFoods');
 
-        app.get('/all-foods', async (req, res) => {
+        app.get('/all-foods', security, async (req, res) => {
             const count = await allFoods.estimatedDocumentCount();
             const result = await allFoods.find().sort({ quantity: -1 }).toArray();
             res.send(result);
         })
-        
+
         app.get('/requested-foods', async (req, res) => {
             const email = req.query.email;
             const result = await requestFoods.find({ userEmail: email }).toArray();
@@ -89,7 +112,7 @@ async function run() {
             if (sort == 'ascending') {
                 const result = await allFoods.find().sort({ expiredDate: 1 }).toArray();
                 return res.send(result)
-            } 
+            }
             const result = await allFoods.find().sort({ expiredDate: -1 }).toArray();
             res.send(result)
         })
@@ -152,6 +175,21 @@ async function run() {
             const id = req.params.id;
             const result = await requestFoods.deleteOne({ _id: new ObjectId(id) });
             res.send(result);
+        })
+
+        app.post('/jwt', (req, res) => {
+            const email = req.body;
+            const token = jwt.sign(email, process.env.SECRET, { expiresIn: '10h' })
+            // console.log(token)
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: false,
+            }).send({ success: token })
+        })
+
+        app.post('/logout', (req, res) => {
+            const user = req.body;
+            res.clearCookie('token', {maxAge: 0}).send({success: true})
         })
 
         // Send a ping to confirm a successful connection
